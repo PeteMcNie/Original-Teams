fb6a5e2f482795196b725bdb884ce98138ec0719
petemcnie <petemcnie0@gmail.com>
2020-06-18 15:01:37 +1200

    Add Player form completed

--- src/client/components/Addplayerform.jsx
+++ src/client/components/Addplayerform.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-const Addplayerform = () => {
-    return (
-        <div>
-            <h3>Form in here</h3>
-        </div>
-    )
+class Addplayerform extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { name: '' }
+    }
+
+    submitHandler = (evnt) => {
+        evnt.preventDefault()
+        // Redirect to Createteams page here
+    }
+
+    changeHandler = (evnt) => {
+        this.setState({ name: evnt.target.value })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submitHandler}>
+                <p>Name:</p>
+                <input type='text' onChange={this.changeHandler} />
+                <input type='submit' value='Add Player' />
+            </form>
+        )
+    }
 }
 
 export default Addplayerform
