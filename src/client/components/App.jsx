af8758c1630062e877551126c9f50d7b10e28f9c
petemcnie <petemcnie0@gmail.com>
2020-06-18 12:40:54 +1200

    Title component created, and linked. Not working currently.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Title from './Title'
 
 const App = () => {
     return (
+      <div>
         <h1>React-Teams</h1>
+        <Route path='/' component={Title} />
+      </div>
     )
 }
 
